e465e7f6701f52e04a2bd0b1b099bd885b3f29d5
Julie <>
2018-05-29 17:38:09 +0200

    update server

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = 3000;
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+
 const api = require('./routes/api');
 const app = express();
 
+
+app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 app.use('/api',api);
+app.use('/public',express.static('./public'));
+app.use(fileUpload());
 
 
 app.get('/', function(req, res){
